c3ddfa840f4c4cee32c12adf9eb3414ea9a0373e
tientd.shinway@gmail.com <asdasdsad>
2015-12-02 02:00:12 +0700

    Add item

---
+++ Assets/_CompletedAssets/Scripts/Items/HealthItem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class HealthItem : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/_CompletedAssets/Scripts/Managers/ItemManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+    public class ItemManager : MonoBehaviour
+    {
+        public PlayerHealth playerHP;
+        public GameObject item;
+        public float spawnItemTime = 15f;            // How long between each spawn.
+        public Transform[] spawnItemPoints;         // An array of the spawn points this item can spawn from.
+
+        float increaseHealth = 50f;                 // increase 50HP 
+        void Start ()
+        {
+            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+            InvokeRepeating("Spawn", spawnItemTime, spawnItemTime);
+        }
+
+
+        void Spawn ()
+        {
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range(0, spawnItemPoints.Length);
+
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate(item, spawnItemPoints[spawnPointIndex].position, spawnItemPoints[spawnPointIndex].rotation);
+        }
+    }
+}
